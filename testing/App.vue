94b7ea78d8fdea24596420d8f8d11264227489e0
Max Walder <max.walder@protonmail.com>
2020-10-15 14:48:39 +0200

    added example/testing app

---
+++ testing/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app" style="height: 750px">
+    <test-component />
+  </div>
+</template>
+
+<script>
+import TestComponent from "./components/TestComponent.vue";
+export default {
+  name: 'app',
+  components: {TestComponent}
+}
+</script>
+
+<style type="text/css">
+  @import "../node_modules/leaflet/dist/leaflet.css";
+</style>
---
+++ testing/components/TestComponent.vue
@@ -0,0 +1,10 @@
+<template>
+  <g-stat-base-map />
+</template>
+<script>
+import GStatBaseMap from "@/lib-components/GStatBaseMap";
+export default {
+  name: 'TestComponent',
+  components: {GStatBaseMap}
+}
+</script>
