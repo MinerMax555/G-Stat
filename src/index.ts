efc3dfa53b534441771b87c3d8408c828016da7c
Max Walder <max.walder@protonmail.com>
2020-11-30 12:41:31 +0100

    1.6.0

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-/* eslint-disable import/prefer-default-export */
 export { default as GStatBaseMap } from './components/GStatBaseMap.vue'
 export { default as GStatAreaLayer } from './components/GStatAreaLayer.vue'
 export { default as GStatMarkerLayer } from './components/GStatMarkerLayer.vue'
--- src/leaflet-markercluster.d.ts
+++ src/leaflet-markercluster.d.ts
@@ -1,1 +1,1 @@
-declare module 'leaflet.markercluster';
+declare module '@glartek/leaflet.markercluster';
